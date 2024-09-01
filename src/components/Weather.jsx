97724b9acd3715ef37581a6a3d9c36c3c1d30d0c
KhushiKhanal4 <khanalkhushi123@gmail.com>
2024-09-01 07:30:03 +0545

    render weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { LuWind } from "react-icons/lu";
+
 import { useLoaderData } from 'react-router-dom'
+import Weatherdetails from './Weatherdetails'
+import { LuWind } from 'react-icons/lu';
+
 
 function Weather() {
     const data = useLoaderData();
@@ -12,10 +15,11 @@ function Weather() {
 
         <div className='bg-gradient-to-tr from-green-400 to-teal-600 '>
 
-            <h2 className='font-medium font-mono text-sm text-white text-shadow-md p-3 sm:py-5 sm:text-lg'>CURRENT WEATHER </h2>
+            <h2 className='font-medium font-sans text-sm text-black text-shadow-md p-3 sm:py-5 sm:text-lg'>CURRENT WEATHER </h2>
+
             <hr className='w-full ' />
 
-            <div className='flex flex-col justify-center justify-items-center items-center pb-9 max-w-screen-md mx-auto sm:flex-row'>
+            <div className='flex flex-col justify-center justify-items-center items-center pb-9 max-w-screen-md mx-auto sm:flex-row lg:max-w-screen-lg lg:gap-9'>
 
                 <div className='flex justify-items-center items-center'>
 
@@ -26,23 +30,39 @@ function Weather() {
 
                     <div className='flex flex-col text-shadow-md'>
 
-                        <h2 className='text-white font-bold font-Kanit text-xl tracking-wider sm:text-3xl'>{tempData.temp} ° C</h2>
+                        <h2 className='text-white font-bold font-Kanit text-xl tracking-wider sm:text-3xl lg:text-5xl'>{tempData.temp} ° C</h2>
 
-                        <h4 className='text-white  font-Kanit text-xs tracking-wider sm:text-sm'>Feels like:{tempData.feels_like} ° C</h4>
+                        <h4 className='text-white  font-Kanit text-xs tracking-wider sm:text-sm lg:text-md'>Feels like:{tempData.feels_like} ° C</h4>
 
                     </div>
 
                 </div>
 
-
-                <div className='bg-[#ffffff38] rounded-lg flex flex-col justify-center max-w-screen-lg p-4'>
-                    <div className='w-full flex justify-between gap-7 items-center'>
-                        <LuWind />
-                        <h1 className='text-white font-Kanit font-semibold text-lg tracking-widest text-shadow'>Wind</h1>
-                        <p>{windData.speed} m/s</p>
-                    </div>
+                <div className='bg-[#ffffff38] rounded-lg flex flex-col justify-center max-w-screen-lg p-4 my-4 sm:m-11 sm:ml-28 lg:p-9'>
+                    <Weatherdetails
+                        icon={<LuWind />}
+                        title={"Wind"}
+                        detail={`${windData.speed} m/s`}
+                    />
+                    <hr />
+                    <Weatherdetails
+                        icon={<LuWind />}
+                        title={"Wind"}
+                        detail={`${windData.speed} m/s`}
+                    />
+                    <hr />
+                    <Weatherdetails
+                        icon={<LuWind />}
+                        title={"Wind"}
+                        detail={`${windData.speed} m/s`}
+                    />
+                    <hr />
+                    <Weatherdetails
+                        icon={<LuWind />}
+                        title={"Wind"}
+                        detail={`${windData.speed} m/s`}
+                    />
                 </div>
-
             </div>
         </div>
     )
---
+++ src/components/Weatherdetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Weatherdetails({
+    icon,
+    title,
+    detail
+}) {
+   
+    return (
+        
+            <div className='w-full flex justify-between gap-9 items-center p-4'>
+                {icon} 
+                <h1 className='text-white font-Kanit font-semibold text-lg tracking-widest text-shadow'>{title}</h1>
+                <p className='text-white font-Kanit font-semibold text-lg tracking-widest text-shadow'>{detail}</p>
+            </div>
+        
+    )
+}
+
+export default Weatherdetails
